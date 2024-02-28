0406c60637f6fbe088ede281765e818e1ca56ad7
kok <kok@koks-MacBook-Pro.local>
2024-02-28 14:25:48 +0300

    logger

---
+++ src/logger/logger.service.ts
@@ -0,0 +1,24 @@
+import { Logger, ILogObj } from 'tslog';
+
+export class LoggerService {
+    public logger: Logger<ILogObj>;
+
+    constructor() {
+        this.logger = new Logger({
+            hideLogPositionForProduction: true,
+            type: 'pretty',
+        });
+    }
+
+    log(...args: unknown[]) {
+        this.logger.info(...args);
+    }
+
+    error(...args: unknown[]) {
+        this.logger.error(...args);
+    }
+
+    warn(...args: unknown[]) {
+        this.logger.warn(...args);
+    }
+}
